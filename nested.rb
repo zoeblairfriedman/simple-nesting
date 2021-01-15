204105a810f2fefc4e1c0a930a91b70dbf64b229
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-15 12:21:54 -0500

    commit tree 04cb3f8c6741b7073d2409bafce2a21fb7ea1ec5

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -16,6 +17,7 @@ def hopper
         }
      }
 
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +39,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-end
+
+     programmer_hash[:alan_kay][:known_for]
+
+    end
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,7 +86,12 @@ def adding_matz
         }
      }
 
-    
+     programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash[:yukihiro_matsumoto] = {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]}
+    programmer_hash
+        
 end
 
 def changing_alan
@@ -101,7 +112,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -122,4 +134,6 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 end
